72ccb933326ad8ce71ffb52222b2e81f0a982ea8
Daniel McCartney <daniel.mccartney@claycountygov.com>
2018-02-24 23:17:38 -0500

    Add project files.

---
+++ GeolocatePermits/Models/ErrorLog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GeolocatePermits.Models
+{
+  public class ErrorLog
+  {
+    public int AppId { get; set; } = 20035;
+    public string ApplicationName { get; set; } = "GeolocatePermits";
+    public string ErrorText { get; set; }
+    public string ErrorMessage { get; set; }
+    public string ErrorStacktrace { get; set; }
+    public string ErrorSource { get; set; }
+    public string Query { get; set; }
+
+    public ErrorLog(string text,
+      string message,
+      string stacktrace,
+      string source,
+      string errorQuery)
+    {
+      ErrorText = text;
+      ErrorMessage = message;
+      ErrorStacktrace = stacktrace;
+      ErrorSource = source;
+      Query = errorQuery;
+      SaveLog();
+    }
+
+    public ErrorLog(Exception ex, string errorQuery = "")
+    {
+      ErrorText = ex.ToString();
+      ErrorMessage = ex.Message;
+      ErrorStacktrace = ex.StackTrace;
+      ErrorSource = ex.Source;
+      Query = errorQuery;
+      SaveLog();
+    }
+
+    private void SaveLog()
+    {
+      string sql = @"
+          INSERT INTO ErrorData 
+          (applicationName, errorText, errorMessage, 
+          errorStacktrace, errorSource, query)  
+          VALUES (@applicationName, @errorText, @errorMessage,
+            @errorStacktrace, @errorSource, @query);";
+
+      using (IDbConnection db = new SqlConnection(Constants.Get_ConnStr(Constants.csError)))
+      {
+        db.Execute(sql, this);
+      }
+    }
+  }
+}
